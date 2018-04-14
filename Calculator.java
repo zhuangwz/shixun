3d71a1a5eb5f78e00d066b2a969ea2a8092b20c0
zhuangwz <zwz1085181077@163.com>
2018-04-14 11:11:05 +0800

    first commit

---
+++ Calculator.java
@@ -0,0 +1,91 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.Console;
+
+public class Calculator {
+    JFrame frame = new JFrame("calculator");
+    JTextField pre = new JTextField();
+    JTextField symbol = new JTextField();
+    JTextField back = new JTextField();
+    JTextField equal = new JTextField();
+    JTextField result = new JTextField();
+    JButton plus = new JButton("+");
+    JButton sub = new JButton("-");
+    JButton mul = new JButton("*");
+    JButton divi = new JButton("/");
+    JButton ok = new JButton("OK");
+
+    public Calculator(){
+        symbol.setEditable(false);
+        equal.setText("=");
+        equal.setEditable(false);
+        result.setEditable(false);
+        pre.setSize(60,60);
+
+        JPanel pan = new JPanel();
+        pan.setLayout(new GridLayout(2,5));
+        pan.add(pre);
+        pan.add(symbol);
+        pan.add(back);
+        pan.add(equal);
+        pan.add(result);
+        pan.add(plus);
+        pan.add(sub);
+        pan.add(mul);
+        pan.add(divi);
+        pan.add(ok);
+        pan.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+
+        frame.setLocation(1000,500);
+        frame.setSize(1000,500);
+        frame.setResizable(false);
+        frame.getContentPane().setLayout(new BorderLayout());
+        frame.getContentPane().add(pan,BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+
+        class Listener implements ActionListener{
+            @SuppressWarnings("unchecked")
+            public void actionPerformed(ActionEvent e){
+                String s = ((JButton)e.getSource()).getText();
+                if(s!="OK")
+                    symbol.setText(s);
+                else{
+                    if (pre.getText() == "" || back.getText() == "")
+                        return;
+                    if(s=="+"){
+                        result.setText(String.valueOf(Double.parseDouble(pre.getText()) + Double.parseDouble(back.getText())));
+                    }
+                    else if(s=="-")
+                        result.setText(String.valueOf(Double.parseDouble(pre.getText()) - Double.parseDouble(back.getText())));
+                    else if(s=="*")
+                        result.setText(String.valueOf(Double.parseDouble(pre.getText()) * Double.parseDouble(back.getText())));
+                    else
+                        result.setText(String.valueOf(Double.parseDouble(pre.getText()) / Double.parseDouble(back.getText())));
+                }
+            }
+        }
+
+        Listener listen = new Listener();
+        plus.addActionListener(listen);
+        sub.addActionListener(listen);
+        mul.addActionListener(listen);
+        divi.addActionListener(listen);
+        ok.addActionListener(listen);
+
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public static void main(String... args){
+        new Calculator();
+    }
+}
---
+++ studyreport.md
@@ -0,0 +1,29 @@
+# VI、Java、Ant和Junit的自学报告
+### VI
+此次我将使用VI编辑器编写一个简单的Java程序。
+进入编辑器后输入i来编写代码，之后键入‘：wq’来保存退出，过程截图如下：
+
+### Java
+我用上面VI编辑器编辑的Hello.java程序进行编译运行，没有出现问题，结果如下图：
+
+
+### 熟悉Ant环境并学习Ant，利用Ant实现Hello的自动编译
+云平台已经配置好了Ant环境，因此我便跳过Ant环境配置这一步。
+下面时我编写的一段xml代码，用来自动编译Hello.java：
+```
+<?xml version="1.0"?>
+<project name="Hello" default="run" basedir="">
+	<target name="compile">
+		<javac srcdir="" />
+	</target>
+
+	<target name="run" depends="compile">
+		<java classname="HelloWorld" />
+	</target>
+</project>
+```
+
+<project>标签中定义了一个<target>标签，该标签表示的就是一个任务，<target>标签中的name属性表示任务名。depends属性则表示在完成“run”任务前要先完成“compile”的任务。
+在终端中键入ant后结果显示编译成功。
+
+### 学习Junit，利用Ant和Junit测试通过HelloWorld
